abd5ade61a1dc63db0472757279d35b2d0f1251a
Swapnil <stamsepublic@gmail.com>
2025-03-29 19:38:38 -0400

    Fix missing imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy, useEffect } from "react";
+import React, { Suspense, lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
